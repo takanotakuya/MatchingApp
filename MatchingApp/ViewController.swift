cac78d15e30153d181be6d871b94db667da9180f
takanotakuya <tmw9898@icloud.com>
2021-06-12 17:27:05 +0900

    stackView使い、画面3分割での表示

--- MatchingApp/ViewController.swift
+++ MatchingApp/ViewController.swift
@@ -12,7 +12,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        let view1 = UIView()
+        view1.backgroundColor = .yellow
+        
+        let view2 = UIView()
+        view2.backgroundColor = .blue
+        
+        let view3 = UIView()
+        view3.backgroundColor = .green
+        
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        
+        self.view.addSubview(stackView)
+        
+        [stackView.topAnchor.constraint(equalTo: view.topAnchor),
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+        stackView.rightAnchor.constraint(equalTo: view.rightAnchor)].forEach { $0.isActive = true }
         
     }
 
