6b1388a107e4483eeac6061ec3d180d800d81d31
takanotakuya <tmw9898@icloud.com>
2021-06-12 18:17:39 +0900

    Bottonクラスの作成、コードの簡略化

--- MatchingApp/Views/BottomControlView.swift
+++ MatchingApp/Views/BottomControlView.swift
@@ -9,41 +9,24 @@ import UIKit
 
 class BottomControlView: UIView {
     
-    let button: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("tap", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .white
-        button.layer.cornerRadius = 25
-        
-        button.layer.shadowOffset = .init(width: 1.5, height: 2)
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.3
-        button.layer.shadowRadius = 15
-        
-        return button
-    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .purple
         
-        let view1 = UIView()
+        let view1 = BottomButtonView()
         view1.backgroundColor = .orange
         
-        view1.addSubview(button)
-        
-        let view2 = UIView()
+        let view2 = BottomButtonView()
         view2.backgroundColor = .orange
         
-        let view3 = UIView()
+        let view3 = BottomButtonView()
         view3.backgroundColor = .orange
         
-        let view4 = UIView()
+        let view4 = BottomButtonView()
         view4.backgroundColor = .orange
         
-        let view5 = UIView()
+        let view5 = BottomButtonView()
         view5.backgroundColor = .orange
         
         let baseStackView = UIStackView(arrangedSubviews: [view1, view2, view3, view4, view5])
@@ -58,16 +41,41 @@ class BottomControlView: UIView {
          baseStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
          baseStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
          baseStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
-        
-         button.centerYAnchor.constraint(equalTo: view1.centerYAnchor),
-         button.centerXAnchor.constraint(equalTo: view1.centerXAnchor),
-         button.widthAnchor.constraint(equalToConstant: 50),
-         button.heightAnchor.constraint(equalToConstant: 50),
-         
-        
         ].forEach { $0.isActive = true }
         
         
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class BottomButtonView: UIView {
+    
+    var button: UIButton?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        button = UIButton(type: .system)
+        button?.setTitle("tap", for: .normal)
+        button?.translatesAutoresizingMaskIntoConstraints = false
+        button?.backgroundColor = .white
+        button?.layer.cornerRadius = 25
+        
+        button?.layer.shadowOffset = .init(width: 1.5, height: 2)
+        button?.layer.shadowColor = UIColor.black.cgColor
+        button?.layer.shadowOpacity = 0.3
+        button?.layer.shadowRadius = 15
+        
+        addSubview(button!)
+        
+        [button?.centerYAnchor.constraint(equalTo: centerYAnchor),
+        button?.centerXAnchor.constraint(equalTo: centerXAnchor),
+        button?.widthAnchor.constraint(equalToConstant: 50),
+        button?.heightAnchor.constraint(equalToConstant: 50)].forEach { $0?.isActive = true }
     }
     
     required init?(coder: NSCoder) {
