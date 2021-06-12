72d874ceadbf5096798a8a42d69fedc810b72df1
takanotakuya <tmw9898@icloud.com>
2021-06-12 17:44:26 +0900

    BottomControlViewの作成

---
+++ MatchingApp/Views/BottomControlView.swift
@@ -0,0 +1,22 @@
+//
+//  BottomControlView.swift
+//  MatchingApp
+//
+//  Created by 髙野拓弥 on 2021/06/12.
+//
+
+import UIKit
+
+class BottomControlView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .purple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
