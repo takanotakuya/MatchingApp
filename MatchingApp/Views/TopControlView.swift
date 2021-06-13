018cee37bcc3afb1a712f1764b844f283b66b480
takanotakuya <tmw9898@icloud.com>
2021-06-13 09:26:56 +0900

    上部ボタン作成

--- MatchingApp/Views/TopControlView.swift
+++ MatchingApp/Views/TopControlView.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class TopControlView: UIView {
     
+    let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("tap", for: .normal)
+        return button
+        
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .purple
+        
+        let baseStackView = UIStackView(arrangedSubviews: [button])
+        baseStackView.axis = .horizontal
+        baseStackView.distribution = .fillEqually
+        baseStackView.spacing = 10
+        baseStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(baseStackView)
+        
+        [baseStackView.topAnchor.constraint(equalTo: topAnchor),
+         baseStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+         baseStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+         baseStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
+        ].forEach { $0.isActive = true }
     }
     
     required init?(coder: NSCoder) {
@@ -20,3 +41,5 @@ class TopControlView: UIView {
     }
     
 }
+
+
