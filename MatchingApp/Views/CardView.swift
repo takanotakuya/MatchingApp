f6e2788d41b8a8a34a43eece20de4c04d83f24be
takanotakuya <tmw9898@icloud.com>
2021-06-13 15:51:20 +0900

    メイン画面作成

---
+++ MatchingApp/Views/CardView.swift
@@ -0,0 +1,85 @@
+//
+//  CardView.swift
+//  MatchingApp
+//
+//  Created by 髙野拓弥 on 2021/06/13.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    let cardImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .blue
+        iv.layer.cornerRadius = 10
+        iv.contentMode = .scaleToFill
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 40, weight: .heavy)
+        label.textColor = .white
+        label.text = "Taro, 22"
+        return label
+        
+    }()
+    
+    let infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "インフォメーションアイコン4")?.resize(size: .init(width: 40, height: 40)), for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        return button
+    }()
+    
+    let residenceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.textColor = .white
+        label.text = "日本, 大阪"
+        return label
+    }()
+    
+    let hobbyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 25, weight: .regular)
+        label.textColor = .white
+        label.text = "ランニング"
+        return label
+    }()
+    
+    let introductionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 25, weight: .regular)
+        label.textColor = .white
+        label.text = "走り回るのが大好きです"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let infoVerticalStackView = UIStackView(arrangedSubviews: [residenceLabel, hobbyLabel, introductionLabel])
+        infoVerticalStackView.axis = .vertical
+        
+        let baseStackView = UIStackView(arrangedSubviews: [infoVerticalStackView, infoButton])
+        baseStackView.axis = .horizontal
+        
+        addSubview(cardImageView)
+        addSubview(nameLabel)
+        addSubview(baseStackView)
+        
+        cardImageView.anchor(top: topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: 10)
+        infoButton.anchor(width: 40)
+        baseStackView.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, right: cardImageView.rightAnchor, bottomPadding: 20, leftPadding: 20, rightPadding: 20)
+        nameLabel.anchor(bottom: baseStackView.topAnchor, left: cardImageView.leftAnchor, bottomPadding: 10, leftPadding: 20)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
