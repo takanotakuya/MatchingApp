338f205d1db27f90f54b1b33f74d36eda7090073
takanotakuya <tmw9898@icloud.com>
2021-06-17 10:34:58 +0900

    RegisterのtextField欄ビューの作成

--- MatchingApp/ViewControllers/RegisterViewController.swift
+++ MatchingApp/ViewControllers/RegisterViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    private let nameTextField = RegisterTextField(plachHolder: "名前")
+    private let emailTextField = RegisterTextField(plachHolder: "email")
+    private let passwordTextField = RegisterTextField(plachHolder: "password")
+    
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("登録", for: .normal)
+        button.backgroundColor = .red
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        return button
+        
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .yellow
+        
+        let baseStackView = UIStackView(arrangedSubviews: [nameTextField, emailTextField, passwordTextField, registerButton])
+        baseStackView.axis = .vertical
+        baseStackView.distribution = .fillEqually
+        baseStackView.spacing = 20
+        
+        view.addSubview(baseStackView)
+        
+        nameTextField.anchor(height: 45)
+        baseStackView.anchor(left: view.leftAnchor, right: view.rightAnchor, centerY: view.centerYAnchor, leftPadding: 40, rightPadding: 40)
     }
     
 }
