dd5cd61ff3bf9e54181b7cae60dfb0fb584d52e7
takanotakuya <tmw9898@icloud.com>
2021-06-17 10:07:01 +0900

    ユーザー登録画面　ファイル作成

---
+++ MatchingApp/ViewControllers/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  MatchingApp
+//
+//  Created by 髙野拓弥 on 2021/06/17.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+    
+}
